2d58809e34597f1262a5dd462facc1a17ebc92c2
Fernan5836 <fernandojavierperezvasquez@gmail.com>
2024-07-29 18:13:26 -0600

    JUnit and Mockito

---
+++ src/main/java/com/SpringBootJUnit/AngelJunittApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelJunittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelJunittApplication.class, args);
+	}
+
+}
